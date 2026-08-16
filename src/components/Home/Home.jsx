8cc691b02237826eaad2cc378b513dabe2cb66cd
hridesh-bharati <hridesh027@gmail.com>
2026-08-16 14:47:53 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <main>
-      {/* Default Bootstrap Carousel with standard controls and indicators */}
+      {/* Bootstrap Carousel with a 2-second delay and navigation buttons removed */}
       <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -34,14 +34,6 @@ export default function Home() {
             <img src="/images/slider1.png" className="d-block w-100 carousel-img" alt="Slide 2" />
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
 
       <div data-aos="fade-up"><Cards /></div>
